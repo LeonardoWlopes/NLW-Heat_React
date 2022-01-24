88347606a2e7191f7255283ff508555d2b6e7454
Leonardo William Lopes <62454440+LeonardoWlopes@users.noreply.github.com>
2022-01-24 17:21:21 -0300

    initial commit

---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import { VscGithubInverted } from "react-icons/vsc";
+import { AuthContext } from "../../contexts/auth";
+
+import styles from "./styles.module.scss";
+
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  };
+};
+
+export function LoginBox() {
+  const { signInUrl, user } = useContext(AuthContext);
+
+
+
+  return (
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href={signInUrl} className={styles.signInWithGithub}>
+        <VscGithubInverted size="24" />
+        Entrar com o GitHub
+      </a>
+    </div>
+  );
+}
